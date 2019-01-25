1f880090b269e16431c1185cdd40dac8080ed5fa
Ancill <ivanfromspace@gmail.com>
2019-01-25 22:57:40 +0300

    change app state to new array set with id prop> delete item

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,34 +3,56 @@ import './todo-list-item.css'
 
 export default class TodoListItem extends Component {
   
-  onLabelClick() {
-    console.log(`Done: ${this.props.label}`);
+  state = {
+      done: false,
+      important: false
+    };
+  
+  onLabelClick = () => {
+    this.setState(({done}) => {
+      return {
+        done: !done
+      };
+    })
+  }
+  onMarkImportant = () => {
+    this.setState(({important}) => { //so if setState going to change => make func with state in  prop (state)
+      return {
+        important: !important
+    };
+  })
   }
 
   render() {
 
-    const { label, important = false } = this.props;
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    const { label, onDeleted} = this.props; //деструктизация от this.props которые передали в параметрах todo-list
+    const { done, important } = this.state;
+
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
+   if (important) {
+     classNames += ' important'
+   }
     
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span
-          className="todo-list-item-label"
-          style={style}
+          className="todo-list-item-label "
           onClick={this.onLabelClick} >
           {label}
         </span>
   
         <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={this.onMarkImportant}>
           <i className="fa fa-exclamation" />
         </button>
   
         <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
+                className="btn btn-outline-danger btn-sm float-right"
+                onClick={onDeleted}>
           <i className="fa fa-trash-o" />
         </button>
       </span>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDom from 'react-dom'
 
 import AppHeader from './components/app-header/app-header'
@@ -6,22 +6,53 @@ import TodoList from './components/todo-list/todo-list'
 import SearchPanel from './components/search-panel/search-panel'
 import ItemStatus from './components/item-status-filter/item-status-filter'
 import './index.css'
-const App = () => {
+export default class App extends Component {
     
-    const todoData = [
-        { label: 'Drink Coffee' , important: false ,id:'23'},
-        { label: 'Clean room' , important: true, id:'43'},
-        { label: 'Learn react' , important: true, id:'45'},
-    ];
-    return (
+    state = {
+        todoData: [
+            { label: 'Drink Coffee' , important: false ,id:'23'},
+            { label: 'Clean room' , important: true, id:'43'},
+            { label: 'Learn react' , important: true, id:'45'},
+        ]
+    };
+
+    deleteItem = (id) => {
+        this.setState(({ todoData }) => {
+            const idx = todoData.findIndex((el) => el.id === id);
+            //todoData.splice(idx,1); //нельзя изменять существующий state!!!!!
+            //теперь нужно сделать так чтобы state не изменялся на прямую, так как это приводит к неразберихи в других модулях
+            // создаем два массива до и за удаленым индексом, а потом соединяем их с помощью spread оператора
+            // const before = todoData.slice(0,idx);
+            // const after = todoData.slice(idx + 1);
+            // const newArray = [...before, ...after]
+            const newArray = [
+                ...todoData.slice(0,idx),
+                ...todoData.slice(idx + 1)
+            ];
+
+
+            return {
+                todoData: newArray
+            }
+
+        })
+    }
+
+    render() {
+        return (
         <div className="todo-app">
             <AppHeader toDo={1} done={2}/> 
             <div className="top-panel d-flex">
                 <SearchPanel />
                 <ItemStatus />
             </div>
-            <TodoList todo={todoData} />
+            <TodoList 
+            todos={this.state.todoData}
+            onDeleted={ (id) => this.deleteItem(id)} 
+            />
         </div>
-)
+        )
+    }
 }
+    
 ReactDom.render(<App/>, document.getElementById("root"))
